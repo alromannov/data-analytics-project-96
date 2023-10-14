ce0a8ddba1129c1f84a3a79466ddb80d2593f6f6
alromannov <135010747+alromannov@users.noreply.github.com>
2023-10-14 19:00:08 +0500

    aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,4 +1,4 @@
-wwith ads as (
+with ads as (
     select
         campaign_date::date,
         utm_source,
@@ -42,16 +42,16 @@ lpc as (
 
 select
     visit_date::date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
+    source,
+    medium,
+    campaign,
     count(visitor_id) as visitors_count,
     total_cost,
     sum(case when lead_id is not null then 1 else 0 end) as leads_count,
     sum(case when s.status_id = 142 then 1 else 0 end) as purchases_count,
     sum(s.amount) as revenue
 from lpc as s
-inner join ads as ad
+left join ads as ad
     on
         s.source = ad.utm_source
         and s.medium = ad.utm_medium and s.campaign = ad.utm_campaign
