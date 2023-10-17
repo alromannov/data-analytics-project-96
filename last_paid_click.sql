2d1934442bb886036f483aad47305405ccc291df
alromannov <135010747+alromannov@users.noreply.github.com>
2023-10-17 14:26:45 +0500

    last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,15 +1,18 @@
 with tab as (
-    select distinct on (visitor_id)
+    select
         s.visitor_id,
         visit_date,
-        source,
-        medium,
-        campaign,
+        source as utm_source,
+        medium as utm_medium,
+        campaign as utm_campaign,
         lead_id,
-        amount,
         created_at,
         closing_reason,
-        status_id
+        status_id,
+        coalesce(amount, 0) as amount,
+        row_number()
+            over (partition by s.visitor_id order by visit_date desc)
+        as rnk
     from sessions as s
     left join leads as l
         on
@@ -18,6 +21,23 @@ with tab as (
     where medium != 'organic'
 )
 
-select * from tab
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from tab
+where rnk = 1
 order by
-    amount desc nulls last, visit_date asc, source asc, medium asc, campaign asc
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
+
